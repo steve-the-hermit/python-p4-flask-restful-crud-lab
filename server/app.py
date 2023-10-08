a501cd8abfb7891c720e6ccc30b9aa1e6db8cd8c
steve-the-hermit <stevemacharia309@gmail.com>
2023-10-08 13:47:45 +0300

    done

--- server/app.py
+++ server/app.py
@@ -47,9 +47,10 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
-
+   
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+  
